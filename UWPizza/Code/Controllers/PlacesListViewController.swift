321eb705ab1653a50453fbe223ad4cb1a57223d7
Vladimir Lyukov <v.lyukov@gmail.com>
2015-08-11 11:21:00 +0300

    Added PlacesService to retrieve data from foursquare API

--- UWPizza/Code/Controllers/PlacesListViewController.swift
+++ UWPizza/Code/Controllers/PlacesListViewController.swift
@@ -7,10 +7,19 @@
 //
 
 import UIKit
+import CoreLocation
 
 
 class PlacesListViewController: UIViewController {
+    lazy var placesService = PlacesService()
+
     @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        placesService.getPlacesForLocation(CLLocation(latitude: 51.5085300, longitude: -0.1257400))
+    }
 
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
---
+++ UWPizza/Code/Services/PlacesService.swift
@@ -0,0 +1,38 @@
+//
+//  PlacesService.swift
+//  UWPizza
+//
+//  Created by Vladimir Lyukov on 11/08/15.
+//  Copyright (c) 2015 upwork. All rights reserved.
+//
+
+import CoreLocation
+import Foundation
+import QuadratTouch
+
+
+class PlacesService {
+    enum Constants {
+        static let limit = 5
+        static let query = "pizza restaurant"
+    }
+
+    func getPlacesForLocation(location: CLLocation) {
+        let parameters = [
+            Parameter.query: Constants.query,
+            Parameter.ll: locationToParam(location),
+            Parameter.limit: Constants.limit.description
+        ]
+
+        let searchTask = Session.sharedSession().venues.search(parameters) { (result: QuadratTouch.Result) in
+            if let response = result.response, let venues = response["venues"] as? [[String:AnyObject]] {
+                println("venues: \(venues)")
+            }
+        }
+        searchTask.start()
+    }
+
+    func locationToParam(location: CLLocation) -> String {
+        return "\(location.coordinate.latitude),\(location.coordinate.longitude)"
+    }
+}
